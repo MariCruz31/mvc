ed476e66accb25c38adaf132ba2b8a83079cd069
maricruz31 <maricruzdev@gmail.com>
2023-07-05 17:09:05 -0300

    Atualizações apis

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express, { json } from 'express';
 import index from './routes/index.js';
 import db from './config/dbConnect.js';
+import apis from './routes/api.js';
 const app = express(); // eata variavel recebe o express, e será usada em outros arquivos
 
 app.use(json()); //começo das conexões 
 app.use("/" , index); //rotear o caminho dentro do arquivo index
+app.use("/apis" , apis);
 
 
 db.on("error" , console.log.bind(console,'Erro de conexão'))
